39b680d4e31409bdb7f47a2f61627f8bd0c4e936
Caaki <95718662+Caaki@users.noreply.github.com>
2022-12-08 19:17:03 +0100

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-import json
-import subprocess
-import sys
-import requests
-import base64
-
-subprocess.check_call([sys.executable, '-m', 'pip', 'install','pynput'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install','requests'])
