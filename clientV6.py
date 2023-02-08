9d2cebf35814d338fe865942a3b6cc28888d620a
Caaki <95718662+Caaki@users.noreply.github.com>
2023-02-08 15:20:35 +0100

    Update clientV6.py

--- clientV6.py
+++ clientV6.py
@@ -21,7 +21,7 @@ def socket_create():
         global spava
         spava = False
         #host = '192.168.8.134'
-        host = '192.168.1.5'
+        host = '192.168.0.22'
         port = 6769
         s = socket.socket()
     except socket.error as msg:
